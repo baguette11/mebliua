4b22f707c1510c484477fcba161258c25c64db65
baguette <matviy1310@gmail.com>
2024-12-20 18:13:00 +0200

    ++

--- js/main.js
+++ js/main.js
@@ -24,8 +24,8 @@ function getCookieValue(cookieName) {
 
 let products_list = document.querySelector(".product-list")
 
-function getCard(product){
-    return  `
+function getCard(product) {
+    return `
     <div class="product-item">
                 <img class='img' src="images/${product.image}" alt="Ліжко">
                 <p>${product.title}</p>
@@ -39,8 +39,8 @@ function getCard(product){
 }
 
 
-class ShoppingCart{
-    constructor(){
+class ShoppingCart {
+    constructor() {
         this.items = {}
         this.loadCartFromCookies()
     }
@@ -59,10 +59,10 @@ class ShoppingCart{
         }
     }
 
-    addItem(product){
-        if (this.items[product.id] ){
+    addItem(product) {
+        if (this.items[product.id]) {
             this.items[product.id].quantity += 1
-        }else{
+        } else {
             this.items[product.id] = product
             this.items[product.id].quantity = 1
         }
@@ -70,9 +70,9 @@ class ShoppingCart{
     }
 }
 
-let cart = new ShoppingCart() 
+let cart = new ShoppingCart()
 
-function addToCard(event){
+function addToCard(event) {
     let data = event.target.getAttribute('data-product')
     let product = JSON.parse(data)
     cart.addItem(product)
@@ -80,25 +80,25 @@ function addToCard(event){
     console.log(cart.items)
 }
 
-getProducts().then(function(products){
+getProducts().then(function (products) {
 
-    if (products_list){
-        products.forEach(function(product){
-            products_list.innerHTML+= getCard(product)
+    if (products_list) {
+        products.forEach(function (product) {
+            products_list.innerHTML += getCard(product)
         })
         let addBtn_list = document.querySelectorAll(".btn-add-to-cart")
-        addBtn_list.forEach(function(btn){
+        addBtn_list.forEach(function (btn) {
             btn.addEventListener("click", addToCard)
         })
     }
-    
+
 })
 
 // Додавання товарів кошика на сторінку
 
 
-function getCartItem(product){
-    return  `
+function getCartItem(product) {
+    return `
          <div class="card my-2">
                 
                     <div class="col-2">
@@ -107,22 +107,38 @@ function getCartItem(product){
                     <div class="col-6">
                         <h5>${product.title}</h5>
                     </div>
-                    <div class="col-2">${product.quantity} шт.</div>
+                    <div class="col-2"><h6>${product.quantity} шт.</h6></div>
                     <div class="col-2">
                         <h4>${product.price * product.quantity} грн</h4>
                     </div>
-                
+                    <div class="remove-btn" data-product='${product.id}'>
+                    <svg  xmlns="http://www.w3.org/2000/svg"  width="24"  height="24"  viewBox="0 0 24 24"  fill="none"  stroke="currentColor"  stroke-width="2"  stroke-linecap="round"  stroke-linejoin="round"  class="icon icon-tabler icons-tabler-outline icon-tabler-trash"><path stroke="none" d="M0 0h24v24H0z" fill="none"/><path d="M4 7l16 0" /><path d="M10 11l0 6" /><path d="M14 11l0 6" /><path d="M5 7l1 12a2 2 0 0 0 2 2h8a2 2 0 0 0 2 -2l1 -12" /><path d="M9 7v-3a1 1 0 0 1 1 -1h4a1 1 0 0 1 1 1v3" /></svg>
+                     </div>
             </div>
     `
 }
 
 let cart_list = document.querySelector(".cart-list")
 
-if (cart_list){
-    cart_list.innerHTML =''
+function showCart(){
+    cart_list.innerHTML = ''
 
-    for (let key in cart.items){
-        cart_list.innerHTML+= getCartItem(cart.items[key])
+    for (let key in cart.items) {
+        cart_list.innerHTML += getCartItem(cart.items[key])
     }
+    let delBtn_list = document.querySelectorAll(".remove-btn")
+    delBtn_list.forEach(function (btn) {
+        btn.addEventListener("click", function (event) {
+            let id = event.currentTarget.getAttribute('data-product')
+            if (id in cart.items) {
+                delete cart.items[id]
+            }
+            cart.saveCartToCookies()
+            showCart()
+        })
+    })
 }
-  
+
+if (cart_list) {
+    showCart()
+}
